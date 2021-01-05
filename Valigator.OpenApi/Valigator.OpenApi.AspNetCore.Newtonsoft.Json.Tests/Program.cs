2c6410e50766139e1644f358bad023a70d7516d2
Nathan Magnus <nathanm@iqmetrix.com>
2021-01-05 16:35:21 -0600

    add nuget packages

---
+++ Valigator.OpenApi/Valigator.OpenApi.AspNetCore.Newtonsoft.Json.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Valigator.OpenApi.AspNetCore.Newtonsoft.Json.Tests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ Valigator.OpenApi/Valigator.OpenApi.AspNetCore.TestWebApp.Integration.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Valigator.OpenApi.AspNetCore.TestWebApp.Integration.Tests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
