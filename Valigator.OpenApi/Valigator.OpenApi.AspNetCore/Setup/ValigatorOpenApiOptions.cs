6abb588b888a8751a927650024cca2744894300b
Nathan Magnus <nathanm@iqmetrix.com>
2021-04-30 13:32:52 -0600

    small documentation

--- Valigator.OpenApi/Valigator.OpenApi.AspNetCore/Setup/ValigatorOpenApiOptions.cs
+++ Valigator.OpenApi/Valigator.OpenApi.AspNetCore/Setup/ValigatorOpenApiOptions.cs
@@ -5,5 +5,8 @@ using Valigator.OpenApi.AspNetCore.Discriminators;
 
 namespace Valigator.OpenApi.AspNetCore.Setup
 {
+	/// <summary>
+	/// Options for Valigator Open API configuration.
+	/// </summary>
 	public record ValigatorOpenApiOptions(string Title, string Version, string Description, Authorization.Authorization[] Authorizations, DiscriminatorMappings DiscriminatorMappings, Action<AspNetCoreOpenApiDocumentGeneratorSettings> ConfigureNswagSettings, ISchemaNameGenerator SchemaNameGenerator);
 }
